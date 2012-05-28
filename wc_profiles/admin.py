7ea9f9adb72cf1da9b44d3109ba4b37550769492
root <root@openstack-ci.(none)>
2012-05-28 22:27:49 +0000

    Added south to pip dependencies and built a first migration. Added Resources model and tweaked the House model.

--- wc_profiles/admin.py
+++ wc_profiles/admin.py
@@ -1,8 +1,10 @@
 from wc_profiles.models import User
 from wc_profiles.models import House
+from wc_profiles.models import Resource
 from wc_profiles.models import Endorsement
 from django.contrib import admin
 
 admin.site.register(User)
 admin.site.register(House)
+admin.site.register(Resource)
 admin.site.register(Endorsement)
--- wc_profiles/models.py
+++ wc_profiles/models.py
@@ -4,12 +4,33 @@ from django.core.files.storage import FileSystemStorage
 
 class House(models.Model):
 	name = models.CharField(max_length=200)
+	summary = models.CharField(max_length=200)
+	created_date = models.DateTimeField('date created')
 	# need custom validator for address
 	address = models.CharField(max_length=400)
-	ammenities = models.TextField()
+	amenities = models.TextField()
 	house_rules = models.TextField()
 	long_term_rooms = models.IntegerField()
 	short_term_rooms = models.IntegerField()
+
+	def __unicode__(self):
+		return (self.name)
+
+
+RESOURCE_TYPES = (
+	('bedroom', 'bedroom'),
+	('bed', 'bed'),
+	('conference_room', 'conference room'),
+)
+
+class Resource(models.Model):
+	house = models.ForeignKey(House)
+	name = models.CharField(max_length=200)
+	blurb = models.CharField(max_length=200)
+	resource_type = models.CharField(max_length=200, choices=RESOURCE_TYPES)
+	def __unicode__(self):
+		return (self.name)
+
 
 class User(models.Model):
 	status_choices = ((0, 'New'), (1, 'Introduced'), (2, 'Accepted'))
