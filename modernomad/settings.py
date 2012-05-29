b9eda381924f34839689d62cfde95d10c1a7d669
root <root@openstack-ci>
2012-05-29 05:06:20 +0000

    Adding custom django-mailman

--- modernomad/settings.py
+++ modernomad/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    'django_mailman',
 )
 
 # A sample logging configuration. The only tangible logging
