3f1c1933aa2b9fb2a0d71ea3143518c6c9ca0da0
Matt Hancher <mdh@maximumentropy.org>
2012-06-03 21:31:16 -0400

    Fix up the URL structure and clean up core.views.

--- core/views.py
+++ core/views.py
@@ -5,23 +5,22 @@ from django.template import RequestContext
 from core.forms import SignupForm
 from core.models import User
 
-def index(request):
-	return HttpResponse("Show all users")
+def ListUsers(request):
+    return HttpResponse("Show all users")
 
-def locations(request):
-	return render_to_response("locations.html")
+def ListLocations(request):
+    return render_to_response("locations.html")
 
-def user(request, user_id):
-	return HttpResponse("This is a specific user page")
+def GetUser(request, user_id):
+   return HttpResponse("This is a specific user page")
 
-def signup(request):
-	if request.method == "POST":
-		f = SignupForm(request.POST, request.FILES) 
-		if f.is_valid():
-			f.save()
-			return HttpResponseRedirect('/thanks')
-	else:
-		f = SignupForm()
-	return render_to_response('signup.html', 
-			{'f': f}, context_instance=RequestContext(request))
-
+def SignupUser(request):
+    if request.method == "POST":
+        f = SignupForm(request.POST, request.FILES) 
+	if f.is_valid():
+	    f.save()
+	    return HttpResponseRedirect('/thanks')
+    else:
+        f = SignupForm()
+    return render_to_response('signup.html', 
+			      {'f': f}, context_instance=RequestContext(request))
