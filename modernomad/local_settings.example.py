bb7993b69747dc87b3e22263a68f770551ff2eb4
Jessy Kate Schingler <jessy@jessykate.com>
2012-07-30 16:24:47 +0200

    add allowed-headers to settings

--- modernomad/local_settings.example.py
+++ modernomad/local_settings.example.py
@@ -2,5 +2,6 @@
 # (ensure local_settings.py is in .gitignore)
 
 # use XS_SHARING_ALLOWED_ORIGINS = '*' for all domains
-XS_SHARING_ALLOWED_ORIGINS = "http://localhost:8989"
-XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
+XS_SHARING_ALLOWED_ORIGINS = "http://localhost:8989/"
+XS_SHARING_ALLOWED_METHODS = ['POST','GET', 'PUT', 'OPTIONS', 'DELETE']
+XS_SHARING_ALLOWED_HEADERS = ["Content-Type"]
