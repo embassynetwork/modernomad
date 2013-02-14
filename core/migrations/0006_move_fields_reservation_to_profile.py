f7ab87f9c7da353c78cffd51ade428ab321f66fb
jessykate <jessy@jessykate.com>
2013-02-14 17:06:14 -0600

    important datamigration update - several fields from reservation model moved to userprofile model for clearer spearation between reservation and personal information.

---
+++ core/migrations/0006_move_fields_reservation_to_profile.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+   	def forwards(self, orm):
+		"Write your forwards methods here."
+		# Note: this 1-1 mapping from reservation to user works easily because
+		# when this migration was applied, no user had more than one
+		# reservation. would need to be more careful if that was not
+		# the case.
+		for reservation in orm.Reservation.objects.all():
+			u = reservation.user
+			profile = orm.UserProfile.objects.filter(user=u)[0]
+			profile.projects = reservation.projects
+			print reservation
+			print profile
+			print reservation.projects
+			print profile.projects
+			profile.sharing = reservation.sharing
+			profile.discussion = reservation.discussion
+			profile.referral = reservation.referral
+			profile.save()
+
+	def backwards(self, orm):
+		"Write your backwards methods here."
+		for reservation in orm.Reservation.objects.all():
+			u = reservation.user
+			profile = orm.UserProfile.objects.filter(user=u)[0]
+			print reservation
+			print profile
+			reservation.projects = profile.projects
+			reservation.sharing = profile.sharing
+			reservation.discussion = profile.discussion
+			reservation.referral = profile.referral
+			reservation.save()
+
+	
+	models = {
+		'auth.group': {
+			'Meta': {'object_name': 'Group'},
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+			'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+		},
+		'auth.permission': {
+			'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+			'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+			'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+		},
+		'auth.user': {
+			'Meta': {'object_name': 'User'},
+			'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+			'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+			'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+			'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+			'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+			'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+			'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+			'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+			'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+			'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+			'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+		},
+		'contenttypes.contenttype': {
+			'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+			'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+			'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+		},
+		'core.house': {
+			'Meta': {'object_name': 'House'},
+			'address': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '400'}),
+			'admins': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+			'amenities': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+			'contact': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
+			'contact_ok': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+			'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+			'description': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+			'events': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+			'events_description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+			'guests': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'latLong': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+			'mission_values': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+			'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+			'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+			'pictures_feed': ('django.db.models.fields.CharField', [], {'max_length': '400', 'null': 'True', 'blank': 'True'}),
+			'residents': ('django.db.models.fields.IntegerField', [], {}),
+			'rooms': ('django.db.models.fields.IntegerField', [], {}),
+			'slug': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+			'space_share': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+			'space_share_description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+			'twitter_handle': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+			'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+			'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+		},
+		'core.reservation': {
+			'Meta': {'object_name': 'Reservation'},
+			'accommodation_preference': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+			'arrival_time': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+			'arrive': ('django.db.models.fields.DateField', [], {}),
+			'comments': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+			'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+			'depart': ('django.db.models.fields.DateField', [], {}),
+			'discussion': ('django.db.models.fields.TextField', [], {}),
+			'guest_emails': ('django.db.models.fields.CharField', [], {'max_length': '400', 'null': 'True', 'blank': 'True'}),
+			'guest_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+			'hosted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'projects': ('django.db.models.fields.TextField', [], {}),
+			'purpose': ('django.db.models.fields.TextField', [], {}),
+			'referral': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+			'sharing': ('django.db.models.fields.TextField', [], {}),
+			'status': ('django.db.models.fields.CharField', [], {'default': "'pending'", 'max_length': '200', 'blank': 'True'}),
+			'tags': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+			'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+			'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+		},
+		'core.resource': {
+			'Meta': {'object_name': 'Resource'},
+			'description': ('django.db.models.fields.TextField', [], {}),
+			'house': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.House']"}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+			'resource_type': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+		},
+		'core.userprofile': {
+			'Meta': {'object_name': 'UserProfile'},
+			'bio': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+			'discussion': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+			'image_thumb': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+			'links': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+			'projects': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+			'referral': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True'}),
+			'sharing': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+			'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+			'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+		}
+	}
+
+	complete_apps = ['core']
+	symmetrical = True
--- core/models.py
+++ core/models.py
@@ -124,13 +124,13 @@ class Reservation(models.Model):
 	tags = models.CharField(max_length =200, help_text='What are 2 or 3 tags that characterize this trip?')
 	guest_emails = models.CharField(max_length=400, blank=True, null=True, 
 		help_text="Comma-separated list of guest emails. A confirmation email will be sent to your guest(s) if you fill this out. (Optional)")
-
-	projects = models.TextField(verbose_name='Current Projects', help_text='Describe one or more projects you are currently working on')
-	sharing = models.TextField(help_text="Is there anything you'd be interested in learning or sharing while you are here?")
-	discussion = models.TextField(help_text="We like discussing thorny issues with each other. What's a question that's been on your mind lately that you don't know the answer to?")
 	purpose = models.TextField(verbose_name='What are you in town for?')
-	referral = models.CharField(max_length=200, verbose_name='How did you hear about us?')
 	comments = models.TextField(blank=True, null=True, verbose_name='Any additional comments')
+
+	# projects = models.TextField(verbose_name='Current Projects', help_text='Describe one or more projects you are currently working on')
+	# sharing = models.TextField(help_text="Is there anything you'd be interested in learning or sharing while you are here?")
+	# discussion = models.TextField(help_text="We like discussing thorny issues with each other. What's a question that's been on your mind lately that you don't know the answer to?")
+	# referral = models.CharField(max_length=200, verbose_name='How did you hear about us?')
 
 	@models.permalink
 	def get_absolute_url(self):
@@ -274,6 +274,11 @@ class UserProfile(models.Model):
 	image_thumb = models.ImageField(upload_to="data/avatars/%Y/%m/%d/", blank=True, null=True)
 	bio = models.TextField("About you", blank=True, null=True)
 	links = models.TextField(help_text="Comma-separated", blank=True, null=True)
+
+	projects = models.TextField(verbose_name='Current Projects', help_text='Describe one or more projects you are currently working on', null=True)
+	sharing = models.TextField(help_text="Is there anything you'd be interested in learning or sharing while you are here?", null=True)
+	discussion = models.TextField(help_text="We like discussing thorny issues with each other. What's a question that's been on your mind lately that you don't know the answer to?", null=True)
+	referral = models.CharField(max_length=200, verbose_name='How did you hear about us?', null=True)
 
 	def __unicode__(self):
 		return (self.user.__unicode__())
