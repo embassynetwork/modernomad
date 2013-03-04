559145c2c8e4e85d19f4a20f4fc88ce124f452ce
jessykate <jessy@jessykate.com>
2013-03-03 16:11:24 -0800

    mockup of new front page

--- modernomad/views.py
+++ modernomad/views.py
@@ -13,7 +13,7 @@ from reservation_calendar import GuestCalendar
 from django.utils.safestring import mark_safe
 
 def index(request):
-	return render(request, "index.html")
+	return render(request, "landing.html")
 	
 def about(request):
 	return render(request, "about.html")
