0ab0d029d7c33930d45fb00024bce3871617730c
jessykate <jessy@jessykate.com>
2013-04-06 17:57:17 -0700

    add an admin menu for house admins and rearrange other links

--- core/templatetags/core_tag_extras.py
+++ core/templatetags/core_tag_extras.py
@@ -1,6 +1,8 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 import itertools
+from django.template import resolve_variable, NodeList
+from django.contrib.auth.models import Group
 
 register = template.Library()
 
@@ -24,3 +26,55 @@ def subsets_size(value, set_size):
             yield chunk
         else:
             break
+
+# from http://djangosnippets.org/snippets/1576/
+@register.tag()
+def ifusergroup(parser, token):
+    """ Check to see if the currently logged in user belongs to a specific
+    group. Requires the Django authentication contrib app and middleware.
+
+    Usage: {% ifusergroup Admins %} ... {% endifusergroup %}, or
+           {% ifusergroup Admins|Group1|Group2 %} ... {% endifusergroup %}, or
+           {% ifusergroup Admins %} ... {% else %} ... {% endifusergroup %}
+
+    """
+    try:
+        tag, group = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("Tag 'ifusergroup' requires 1 argument.")
+    
+    nodelist_true = parser.parse(('else', 'endifusergroup'))
+    token = parser.next_token()
+    
+    if token.contents == 'else':
+        nodelist_false = parser.parse(('endifusergroup',))
+        parser.delete_first_token()
+    else:
+        nodelist_false = NodeList()
+    
+    return GroupCheckNode(group, nodelist_true, nodelist_false)
+
+
+class GroupCheckNode(template.Node):
+    def __init__(self, group, nodelist_true, nodelist_false):
+        self.group = group
+        self.nodelist_true = nodelist_true
+        self.nodelist_false = nodelist_false
+    def render(self, context):
+        user = resolve_variable('user', context)
+        
+        if not user.is_authenticated():
+            return self.nodelist_false.render(context)
+            
+        try:
+            for group in self.group.split("|"):
+                
+                if Group.objects.get(name=group) in user.groups.all():
+                    return self.nodelist_true.render(context)
+
+        except Group.DoesNotExist:
+            return self.nodelist_false.render(context)
+            
+        
+        return self.nodelist_false.render(context)
+
