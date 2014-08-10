79fae322cad103374bae6d525567eae41ea26c28
jessykate <jessy@jessykate.com>
2014-08-10 02:48:06 -0700

    work on index and location templates

--- modernomad/views.py
+++ modernomad/views.py
@@ -1,16 +1,34 @@
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.shortcuts import render
-from core.models import Location
+from core.models import Location, UserProfile
 from gather.models import Event
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils.safestring import SafeString
 from django.conf import settings
-
+import json
+from django.utils.html import strip_tags
 
 def index(request):
-	return render(request, "index.html")
+	recent_events = Event.objects.order_by('-start')[:10]
+	locations = Location.objects.all()
+	location_list = []
+	for location in locations:
+		location_list.append({
+				'name': location.name,
+				'latitude': location.latitude,
+				'longitude': location.longitude,
+				'short_description': strip_tags(location.short_description[:200])+"...",
+				'image': location.image.url,
+				'stay_url': location.get_absolute_url() + "stay",
+				'has_availability': location.has_availability()
+		})
+	print location_list
+	# (JKS) the exclusions here are shenanigans to ensure we exclude older
+	# profiles with missing images or wierd migration issues 
+	random_profiles = UserProfile.objects.exclude(image__endswith='default.jpg').exclude(image='').order_by('?')[:90]
+	return render(request, "index.html", {'location_list': json.dumps(location_list), 'recent_events': recent_events, 'random_profiles': random_profiles})
 
 def about(request):
 	return render(request, "about.html")
