f7c9fe832ed73f3223c3d9d7805052f6c7e2e0c6
Jacob Sayles <jacob@officenomads.com>
2014-08-14 13:51:58 -0700

    fixed type-o bug

--- core/reservation_calendar.py
+++ core/reservation_calendar.py
@@ -42,7 +42,7 @@ class GuestCalendar(HTMLCalendar):
 						room_type = "P"
 
 					body.append('<li id="res%d-cal-item">' %reservation.id)
-					if reservation.is_apprived():
+					if reservation.is_approved():
 						body.append('<a href="#reservation%d" class="greyed-out">' % reservation.id)
 					else:
 						body.append('<a href="#reservation%d">' % reservation.id)
