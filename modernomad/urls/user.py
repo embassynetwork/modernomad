c09ec7255349b5b9f522fa4b1b1975adefc8afff
Jacob Sayles <jacob@officenomads.com>
2014-08-14 16:08:31 -0700

    tabs not spaces

--- modernomad/urls/user.py
+++ modernomad/urls/user.py
@@ -8,18 +8,17 @@ import core.forms
 # django-registration package, overriding the initial registration
 # view to collect our additional user profile information.
 urlpatterns = patterns('',
-    url(r'^register/$', Registration.as_view(form_class = core.forms.UserProfileForm),
-		name='registration_register'),
+	url(r'^register/$', Registration.as_view(form_class = core.forms.UserProfileForm), name='registration_register'),
 )
 urlpatterns += registration.backends.default.urls.urlpatterns
 
 urlpatterns += patterns('core.views',
-    url(r'^$', 'ListUsers', name='user_list'),
-    url(r'^daterange/$', 'PeopleDaterangeQuery', name='people_daterange'),
-    url(r'^(?P<username>(?!logout)(?!login)(?!register)[\w\d\-\.@+_]+)/$', 'GetUser', name='user_detail'),
-    url(r'^(?P<username>[\w\d\-\.@+_]+)/edit/$', 'UserEdit', name='user_edit'),
-    url(r'^(?P<username>[\w\d\-\.@+_]+)/addcard/$', 'UserAddCard', name='user_add_card'),
-    url(r'^(?P<username>[\w\d\-\.@+_]+)/deletecard/$', 'UserDeleteCard', name='user_delete_card'),
+	url(r'^$', 'ListUsers', name='user_list'),
+	url(r'^daterange/$', 'PeopleDaterangeQuery', name='people_daterange'),
+	url(r'^(?P<username>(?!logout)(?!login)(?!register)[\w\d\-\.@+_]+)/$', 'GetUser', name='user_detail'),
+	url(r'^(?P<username>[\w\d\-\.@+_]+)/edit/$', 'UserEdit', name='user_edit'),
+	url(r'^(?P<username>[\w\d\-\.@+_]+)/addcard/$', 'UserAddCard', name='user_add_card'),
+	url(r'^(?P<username>[\w\d\-\.@+_]+)/deletecard/$', 'UserDeleteCard', name='user_delete_card'),
 )
 
 # XXX can this be extracted and put into the gather app?
