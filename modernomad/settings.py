1a79b772ef18309bb51b3f262ed4c845017f4da9
Jacob Sayles <jacob@officenomads.com>
2014-08-15 09:37:55 -0700

    added new BACKUP_ROOT

--- modernomad/settings.py
+++ modernomad/settings.py
@@ -5,6 +5,8 @@ import os
 ROOT = os.path.dirname(os.path.abspath(__file__))
 path = lambda *a: os.path.join(ROOT, *a)
 
+BACKUP_ROOT = ROOT + '/backups/'
+
 ADMINS = (
     ('Jessy Kate Schingler', 'jessy@embassynetwork.com'),
 )
