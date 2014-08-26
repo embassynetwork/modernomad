5eb3f424ddfbb4b8bb3409761948a48cc2bc7a67
Jacob Sayles <jacob@officenomads.com>
2014-08-26 10:54:57 -0700

    hooked up stripe refund

--- core/payment_gateway.py
+++ core/payment_gateway.py
@@ -22,10 +22,18 @@ def charge_card(reservation):
 		raise PaymentException("No payment system configured")
 
 def issue_refund(payment):
-	if settings.STRIPE_SECRET_KEY:
+	if payment.payment_service == "Stripe" and settings.STRIPE_SECRET_KEY:
 		return stripe_issue_refund(payment)
-	elif settings.USA_E_PAY_KEY:
+	elif payment.payment_service == "USAePay" and settings.USA_E_PAY_KEY:
 		return usaepay_issue_refund(payment)
+	elif not payment.payment_service == "Stripe" and not payment.payment_service == "USAePay":
+		logger.info("issue_refund: Payment not issued through service so we can't refund it.")
+		return Payment.objects.create(reservation=payment.reservation,
+			payment_service = payment.payment_service,
+			paid_amount = -1 * payment.paid_amount,
+			payment_method = "Refund",
+			transaction_id = "Manual",
+		)
 	else:
 		raise PaymentException("No payment system configured")
 
@@ -34,7 +42,7 @@ def issue_refund(payment):
 ###################################################################
 
 def stripe_charge_card(reservation):
-	logger.debug("stripe_charge_card(reservation=%s)" + str(reservation.id))
+	logger.debug("stripe_charge_card(reservation=%s)" % reservation.id)
 	
 	# stripe will raise a stripe.CardError if the charge fails. this
 	# function purposefully does not handle that error so the calling
@@ -51,7 +59,7 @@ def stripe_charge_card(reservation):
 			currency="usd",
 			customer = reservation.user.profile.customer_id,
 			description=descr
-			)
+		)
 
 	# Store the charge details in a Payment object
 	return Payment.objects.create(reservation=reservation,
@@ -62,18 +70,28 @@ def stripe_charge_card(reservation):
 	)
 
 def stripe_issue_refund(payment):
-	# https://stripe.com/docs/api#create_refund
-	logger.debug("stripe_issue_refund(payment=%s)" + payment.id)
-	return None
+	logger.debug("stripe_issue_refund(payment=%s)" % payment.id)
+	
+	stripe.api_key = settings.STRIPE_SECRET_KEY
+	charge = stripe.Charge.retrieve(payment.transaction_id)
+	refund = charge.refunds.create()
+
+	# Store the charge details in a Payment object
+	return Payment.objects.create(reservation=reservation,
+		payment_service = "Stripe",
+		payment_method = "Refund",
+		paid_amount = -1 * payment.paid_amount,
+		transaction_id = refund.id
+	)
 	
 ###################################################################
 # USAePay Methods
 ###################################################################
 
 def usaepay_charge_card(reservation):
-	logger.debug("usaepay_charge_card(reservation=%s)" + str(reservation.id))
+	logger.debug("usaepay_charge_card(reservation=%s)" % reservation.id)
 	return None
 
 def usaepay_issue_refund(payment):
-	logger.debug("usaepay_issue_refund(payment=%s)" + payment.id)
+	logger.debug("usaepay_issue_refund(payment=%s)" % payment.id)
 	return None
