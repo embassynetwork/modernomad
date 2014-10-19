ab1f6f7e978840a3e112be6a5b1565ea2fb4ad52
Jacob Sayles <jacob@officenomads.com>
2014-10-19 13:10:46 -0700

    new command to set all user emails, passwors, and customer_ids for testing

---
+++ modernomad/management/commands/set_all_users.py
@@ -0,0 +1,37 @@
+import os
+import time
+import urllib
+import sys
+import datetime
+
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+	help = "Set all user email addresses to the one given, and password to 'password'"
+	args = "[email_address, password, customer_id]"
+	requires_model_validation = True
+
+	def handle(self, *labels, **options):
+		if not labels or len(labels) < 1: raise CommandError('Args: <email_address>, <password>, <customer_id>')
+		new_email_address = labels[0]
+		print "Setting all emails:'%s' " % new_email_address
+		new_password = None
+		if len(labels) >= 2: 
+			new_password = labels[1]
+			print "Setting all passwords:'%s' " % new_password
+		new_customer_id = None
+		if len(labels) >= 3: 
+			new_customer_id = labels[2]
+			print "Setting all customer ids:'%s' " % new_customer_id
+
+		changes = 0
+		for u in User.objects.all():
+			u.email = new_email_address
+			if new_password:
+				u.set_password(new_password)
+			if new_customer_id:
+				u.profile.customer_id = new_customer_id
+			u.save()
+			changes = changes + 1
+		print "Changed %s users." % changes
