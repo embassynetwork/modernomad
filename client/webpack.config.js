525ae15bd57ee3fad80fdec99e0c5965351a0105
jessykate <jessy@jessykate.com>
2016-08-06 21:24:37 +0200

    hot reloading hotness

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,19 +4,27 @@ var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
   context: __dirname,
-  entry: './js/index',
+  entry: [ 'webpack-dev-server/client?http://localhost:3000',
+			'webpack/hot/only-dev-server', 
+			'./js/index'
+		],
   output: {
       path: path.resolve('./build/'),
-      filename: "[name]-[hash].js"
+      filename: "[name]-[hash].js",
+	  //does this need to match our build/ directory?
+	  //publicPath: 'http://localhost:3000/assets/bundles/', // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
+	  publicPath: 'http://localhost:3000/build/', // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
   },
 
   plugins: [
+	new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(), // don't reload if there is an error
     new BundleTracker({filename: './webpack-stats.json'})
   ],
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'}, // to transform JSX into JS
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader'], }, // to transform JSX into JS
     ],
   },
 
