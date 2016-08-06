6052fe9bf6471ddb8df9f3730aa6513353ac8985
craigambrose <craig@craigambrose.com>
2016-08-06 17:54:01 +0200

    ready for babel

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,16 @@ module.exports = {
 
   plugins: [
     new BundleTracker({filename: './webpack-stats.json'})
-  ]
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'}, // to transform JSX into JS
+    ],
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js', '.jsx']
+  }
 }
