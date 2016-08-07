4880ac564430ae854eaedbe7b525716dc4058801
craigambrose <craig@craigambrose.com>
2016-08-07 12:13:26 +0200

    added wrapper container to hold state

---
+++ client/js/AvailabilityContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AvailabilityTable from './AvailabilityTable'
+import { clone } from 'lodash'
+
+export default class AvailabilityContainer extends React.Component {
+  constructor(props) {
+    super({})
+    this.state = clone(props)
+  }
+
+  render() {
+    return <AvailabilityTable availabilities={this.state.availabilities} />;
+  }
+}
--- client/js/index.jsx
+++ client/js/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import AvailabilityTable from './AvailabilityTable'
+import AvailabilityContainer from './AvailabilityContainer'
 
 var topLevelComponents = {}
-topLevelComponents.AvailabilityTable = AvailabilityTable;
+topLevelComponents.AvailabilityContainer = AvailabilityContainer;
 
 window.renderReactComponent = function(id, componentName, props) {
   var component = topLevelComponents[componentName];
