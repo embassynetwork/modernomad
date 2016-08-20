63906a9cdb4866b4b2c87f6f659a06f0ab400df4
craigambrose <craig@craigambrose.com>
2016-08-20 22:10:48 +0200

    working on tests

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 
 from api.views.location import *
 from api.views.availabilities import *
-from api.serializers import *
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
