822e0671aeb35c21c37ee5de8d970045daaea330
craigambrose <craig@craigambrose.com>
2016-08-25 13:37:25 +0300

    test fix for quantity_on

--- core/tests/test_availabilities.py
+++ core/tests/test_availabilities.py
@@ -13,22 +13,27 @@ class AvailabilityQuantityOnTestCase(TestCase):
         return Availability.objects.create(resource=self.resource, start_date=date, quantity=quantity)
 
     def test_quantity_is_zero_with_no_availabilities(self):
-        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 13)), 0)
+        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 13), self.resource), 0)
 
     def test_quantity_is_zero_before_availabilities(self):
         self.create_on(date(4016, 1, 14), 3)
-        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 13)), 0)
+        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 13), self.resource), 0)
 
     def test_quantity_after_availability(self):
         self.create_on(date(4016, 1, 14), 3)
-        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 14)), 3)
-        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 15)), 3)
+        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 14), self.resource), 3)
+        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 15), self.resource), 3)
+
+    def test_quantity_doesnt_include_other_resource(self):
+        other_resource = ResourceFactory(location=self.resource.location)
+        self.create_on(date(4016, 1, 14), 3)
+        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 14), other_resource), 0)
 
     def test_quantity_with_two_availabilities(self):
         self.create_on(date(4016, 1, 14), 3)
         self.create_on(date(4016, 1, 16), 1)
-        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 13)), 0)
-        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 14)), 3)
-        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 15)), 3)
-        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 16)), 1)
-        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 17)), 1)
+        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 13), self.resource), 0)
+        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 14), self.resource), 3)
+        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 15), self.resource), 3)
+        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 16), self.resource), 1)
+        self.assertEqual(Availability.objects.quantity_on(date(4016, 1, 17), self.resource), 1)
