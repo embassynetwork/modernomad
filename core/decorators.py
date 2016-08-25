78ab492b01ee50c036de8440db5d180717096175
craigambrose <craig@craigambrose.com>
2016-08-25 14:54:51 +0300

    pep8 :)

--- core/decorators.py
+++ core/decorators.py
@@ -4,6 +4,7 @@ from core.models import get_location
 from functools import wraps
 from django.conf import settings
 
+
 def group_required(*group_names):
     """Requires user membership in at least one of the groups passed in."""
     def in_groups(u):
@@ -13,36 +14,44 @@ def group_required(*group_names):
         return False
     return user_passes_test(in_groups)
 
+
 def house_admin_required(original_func):
-	@wraps(original_func)
-	def decorator(request, location_slug, *args, **kwargs):
-		location = get_location(location_slug)
-		user = request.user
-		if user.is_authenticated() and location and user in location.house_admins.all():
-			return original_func(request, location_slug, *args, **kwargs)
-		elif request.user.is_authenticated():
-			return HttpResponseRedirect("/")
-		else: 
-			from django.contrib.auth.views import redirect_to_login
-			path = request.get_full_path()
-			login_url = settings.LOGIN_URL
-			return redirect_to_login(path, login_url)
-	return decorator 
+    @wraps(original_func)
+    def decorator(request, location_slug, *args, **kwargs):
+        location = get_location(location_slug)
+        user = request.user
+        if user.is_authenticated() and location and user in location.house_admins.all():
+            return original_func(request, location_slug, *args, **kwargs)
+        elif request.user.is_authenticated():
+            return HttpResponseRedirect("/")
+        else:
+            from django.contrib.auth.views import redirect_to_login
+            path = request.get_full_path()
+            login_url = settings.LOGIN_URL
+            return redirect_to_login(path, login_url)
+    return decorator
+
 
 def resident_or_admin_required(original_func):
-	@wraps(original_func)
-	def decorator(request, location_slug, *args, **kwargs):
-		location = get_location(location_slug)
-		user = request.user
-		if user.is_authenticated() and location and (user in location.residents.all() or user in location.house_admins.all() or user in location.readonly_admins.all()):
-			return original_func(request, location_slug, *args, **kwargs)
-		elif request.user.is_authenticated():
-			return HttpResponseRedirect("/")
-		else: 
-			from django.contrib.auth.views import redirect_to_login
-			path = request.get_full_path()
-			login_url = settings.LOGIN_URL
-			return redirect_to_login(path, login_url)
-	return decorator 
-
-
+    @wraps(original_func)
+    def decorator(request, location_slug, *args, **kwargs):
+        location = get_location(location_slug)
+        user = request.user
+        if (
+            user.is_authenticated() and
+            location and
+            (
+                user in location.residents.all() or
+                user in location.house_admins.all() or
+                user in location.readonly_admins.all()
+            )
+        ):
+            return original_func(request, location_slug, *args, **kwargs)
+        elif request.user.is_authenticated():
+            return HttpResponseRedirect("/")
+        else:
+            from django.contrib.auth.views import redirect_to_login
+            path = request.get_full_path()
+            login_url = settings.LOGIN_URL
+            return redirect_to_login(path, login_url)
+    return decorator
