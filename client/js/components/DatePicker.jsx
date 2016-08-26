0c8c7ec383cd9e6273bde566c95041349a5b304e
jessykate <jessy@jessykate.com>
2016-08-26 16:32:58 +0200

    disable dates in the past on jquery datepicker

--- client/js/components/DatePicker.jsx
+++ client/js/components/DatePicker.jsx
@@ -5,6 +5,7 @@ export default class DatePicker extends React.Component {
 	componentDidMount() {
 		const element = ReactDom.findDOMNode(this)
 		$(element).datepicker({
+			minDate: 0,
 			dateFormat: "yy-mm-dd"
 		})
     }
