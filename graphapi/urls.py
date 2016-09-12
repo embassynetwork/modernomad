77fa6717c8704c321fadd541d0dec1d762371e4c
craigambrose <craig@craigambrose.com>
2016-09-12 09:26:27 -0700

    GraphQL endpoint accepts JWT auth

--- graphapi/urls.py
+++ graphapi/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
+from jwt_auth.mixins import JSONWebTokenAuthMixin
 
 from graphene.contrib.django.views import GraphQLView
 
 from graphapi.schema import schema
 
+
+class AuthGraphQLView(JSONWebTokenAuthMixin, GraphQLView):
+    pass
+
 urlpatterns = [
-    url(r'^graphql', csrf_exempt(GraphQLView.as_view(schema=schema))),
+    url(r'^graphql', csrf_exempt(AuthGraphQLView.as_view(schema=schema))),
     url(r'^graphiql', include('django_graphiql.urls')),
 ]
