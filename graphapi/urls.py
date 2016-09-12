f888192a26b36a6fcf8c6aad02ff05b8266706fa
craigambrose <craig@craigambrose.com>
2016-09-12 13:14:49 -0700

    JWT auth is optional

--- graphapi/urls.py
+++ graphapi/urls.py
@@ -2,13 +2,21 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
 from jwt_auth.mixins import JSONWebTokenAuthMixin
-
 from graphene.contrib.django.views import GraphQLView
-
 from graphapi.schema import schema
+from jwt_auth.utils import get_authorization_header
 
 
-class AuthGraphQLView(JSONWebTokenAuthMixin, GraphQLView):
+class OptionalJWTMixin(JSONWebTokenAuthMixin):
+    def dispatch(self, request, *args, **kwargs):
+        auth = get_authorization_header(request)
+        if auth:
+            return super(OptionalJWTMixin, self).dispatch(request, *args, **kwargs)
+        else:
+            return super(JSONWebTokenAuthMixin, self).dispatch(request, *args, **kwargs)
+
+
+class AuthGraphQLView(OptionalJWTMixin, GraphQLView):
     pass
 
 urlpatterns = [
