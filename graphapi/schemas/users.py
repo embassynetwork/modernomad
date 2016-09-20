2dd6b3e3b443e6420cb58c2472732a574f5bf769
craigambrose <craig@craigambrose.com>
2016-09-20 15:55:21 +1200

    return some user profile information

--- graphapi/schemas/users.py
+++ graphapi/schemas/users.py
@@ -4,10 +4,18 @@ from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from django.contrib.auth.models import User
+from core.models import UserProfile
+
+
+class UserProfileNode(DjangoObjectType):
+    class Meta:
+        model = UserProfile
+        interfaces = (Node, )
 
 
 class UserNode(DjangoObjectType):
     name = graphene.String()
+    status = graphene.String()
 
     class Meta:
         model = User
@@ -15,6 +23,10 @@ class UserNode(DjangoObjectType):
 
     def resolve_name(self, args, *_):
         return " ".join([self.first_name, self.last_name])
+
+    # This really needs to go on some booking object and be properly calculated
+    def resolve_status(self, args, *_):
+        return "guest"
 
 
 class Query(AbstractType):
