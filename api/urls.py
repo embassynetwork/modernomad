ef5d3fcfe60e00574522ccd1ebbe6186479b74de
craigambrose <craig@craigambrose.com>
2016-09-25 14:33:11 +1300

    another travis fix

--- api/urls.py
+++ api/urls.py
@@ -13,7 +13,4 @@ urlpatterns = patterns(
     url(r'^availabilities/$', availabilities),
     url(r'^availability/(?P<availability_id>[0-9]+)$', availability_detail),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(
-        r'user/(?P<username>[\w\d\-\.@+_]+)/current_location/occupancies.json$',
-        CurrentLocationOccupancies.as_view(), name='current_location_occupancies'),
 )
