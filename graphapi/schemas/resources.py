8080b73437a3088963e4f60dc818f7754810221c
craigambrose <craig@craigambrose.com>
2016-09-29 20:19:17 +1300

    schema supports dummy usabilities

--- graphapi/schemas/resources.py
+++ graphapi/schemas/resources.py
@@ -1,13 +1,20 @@
 import graphene
 from graphene import AbstractType, Field, Node
+from graphene.types.datetime import *
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from core.models import Resource
 
 
+class UsabilityNode(graphene.ObjectType):
+    date = DateTime()
+    quantity = graphene.Int()
+
+
 class ResourceNode(DjangoObjectType):
     rid = graphene.Int()
+    usabilities = graphene.List(lambda: UsabilityNode, arrive=DateTime(), depart=DateTime())
 
     class Meta:
         model = Resource
@@ -16,10 +23,12 @@ class ResourceNode(DjangoObjectType):
             'location': ['exact'],
             'location__slug': ['exact'],
         }
-        # filter_order_by = ['slug']
 
     def resolve_rid(self, args, *_):
         return self.id
+
+    def resolve_usabilities(self, args, *stuff):
+        return [UsabilityNode(args['arrive'], 12)]
 
 
 class Query(AbstractType):
