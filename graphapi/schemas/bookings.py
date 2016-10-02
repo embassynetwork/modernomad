767ad0a37912f0b1d461870623deab4857df1b22
craigambrose <craig@craigambrose.com>
2016-10-03 09:34:18 +1300

    booking mutation creates a booking and use

--- graphapi/schemas/bookings.py
+++ graphapi/schemas/bookings.py
@@ -7,23 +7,40 @@ from graphene.types.datetime import *
 
 from api.commands.bookings import RequestBooking
 from graphapi.schemas.resources import ResourceNode
-from core.models import Resource
+from core.models import Resource, Booking
+
+
+def commandErrorsToGraphQL(errors):
+    result = []
+    for field_name, messages in errors.iteritems():
+        for message in messages:
+            result.append([field_name, message])
+    return result
+
+
+class BookingNode(DjangoObjectType):
+    class Meta:
+        model = Booking
+        interfaces = (Node, )
 
 
 class RequestBookingMutation(graphene.Mutation):
     class Input:
         arrive = DateTime(required=True)
         depart = DateTime(required=True)
-        resource_id = graphene.String(required=True)
+        resource = graphene.String(required=True)
+        purpose = graphene.String(required=False)
+        arrival_time = graphene.String(required=False)
+        comments = graphene.String(required=False)
 
-    name = graphene.String()
+    booking = graphene.Field(BookingNode)
+    errors = graphene.List(graphene.List(graphene.String))
+    ok = graphene.Boolean()
 
     @classmethod
     def mutate(cls, root, data, context, info):
-        # form = BookingUseForm(None, args)
-        # if form.is_valid():
-        #     return CreateBooking(name=str(valid))
-        # else:
         command = RequestBooking(context.user, **data)
-        command.execute()
-        return RequestBookingMutation(name=str(command.result()))
+        if command.execute():
+            return RequestBookingMutation(ok=True, booking=command.result().data.get('booking'))
+        else:
+            return RequestBookingMutation(ok=False, errors=commandErrorsToGraphQL(command.result().errors))
