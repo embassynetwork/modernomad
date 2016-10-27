a62f08cc95d40686dbe05844c00cfeb39a0a2f1d
jessykate <jessy@jessykate.com>
2016-10-27 15:11:20 -0700

    rss feed for public events per location

---
+++ gather/syndication.py
@@ -0,0 +1,33 @@
+from django_ical.views import ICalFeed
+from django.core.urlresolvers import reverse
+from gather.models import Event
+from core.models import Location
+from django.shortcuts import get_object_or_404
+
+class PublicEventsFeed(ICalFeed):
+    product_id = '-//embassynetwork.com//redvic//publicevents'
+    timezone = 'PST'
+    file_name = "events.ics"
+
+    def get_object(self, request, location_slug):
+        return get_object_or_404(Location, slug=location_slug)
+
+    def items(self, obj):
+        rv = Location.objects.get(slug="redvic")
+        return Event.objects.filter(location=obj).filter(status=Event.CONFIRMED).filter(visibility=Event.PUBLIC).order_by('-start')
+
+    def item_title(self, obj):
+        return obj.title
+
+    def item_description(self, obj):
+        return obj.description
+
+    def item_start_datetime(self, obj):
+        return obj.start
+
+    def item_end_datetime(self, obj):
+        return obj.end
+
+    def item_link(self, obj):
+        return reverse('gather_view_event', args=[obj.location.slug, obj.pk, obj.slug])
+
--- gather/urls.py
+++ gather/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from gather.syndication import PublicEventsFeed
 
 urlpatterns = patterns('',
     url(r'^create/$', 'gather.views.create_event', name='gather_create_event'),
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^past/$', 'gather.views.past_events', name='gather_past_events'),
     url(r'^review/$', 'gather.views.needs_review', name='gather_needs_review'),
     url(r'^message/$', 'gather.emails.event_message', name='gather_event_message'),
+    url(r'^latest/feed.ics/$', PublicEventsFeed()), # <-- url args are passed to the feed class
     url(r'^emailpreferences/(?P<username>[\w\d\-\.@+_]+)/$', 'gather.views.email_preferences', name='gather_email_preferences'),
     url(r'^(?P<event_id>\d+)/(?P<event_slug>[\w\d\-\.@+_]+)/edit/$', 'gather.views.edit_event', name='gather_edit_event'),
     url(r'^(?P<event_id>\d+)/(?P<event_slug>[\w\d\-\.@+_]+)/rsvp/yes/newuser/$', 'gather.views.rsvp_new_user', name='gather_rsvp_new_user'),
