d21b0843c34f6d51a400627ba857b7313cabd06d
jessykate <jessy@jessykate.com>
2017-02-10 16:29:01 +1300

    1.10 migration woes

--- modernomad/settings.py
+++ modernomad/settings.py
@@ -190,12 +190,12 @@ TEMPLATES = [
             # default template context processors
             'context_processors' : [
                 "django.contrib.auth.context_processors.auth",
-                "django.core.context_processors.debug",
-                "django.core.context_processors.i18n",
-                "django.core.context_processors.media",
-                "django.core.context_processors.static",
-                "django.core.context_processors.tz",
-                "django.core.context_processors.request",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.template.context_processors.request",
                 "django.contrib.messages.context_processors.messages",
                 "core.context_processors.location.location_variables",
                 "core.context_processors.location.network_locations",
