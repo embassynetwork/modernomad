6825982c7e02ec5298ed69e0a99674ea8a84cc1e
Ben Firshman <ben@firshman.co.uk>
2017-10-10 13:20:06 +0300

    Make Docker image work in production

--- modernomad/wsgi.py
+++ modernomad/wsgi.py
@@ -21,8 +21,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "modernomad.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+application = DjangoWhiteNoise(application)
