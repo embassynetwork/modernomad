5d0cbe4c6f625eaa492de5026214598dc26030fb
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2018-10-22 19:20:03 +0200

    Modify new settings, hattip to @bfirsh

--- modernomad/settings/production.py
+++ modernomad/settings/production.py
@@ -1,7 +1,29 @@
-from .common import *
+from .common import *　# noqa
+from .common import env
+from .common import BASE_DIR
 
-ALLOWED_HOSTS = [
-    'www.embassynetwork.com',
-    'embassynetwork.com',
-    'map.chozadelmundo.com'
-]
+SECRET_KEY = env('SECRET_KEY')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': env('DJANGO_LOG_LEVEL', default='INFO'),
+        },
+    },
+}
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': '',
+        'CACHE': True,
+        'STATS_FILE': BASE_DIR / 'client/webpack-stats-prod.json',
+    }
+}
