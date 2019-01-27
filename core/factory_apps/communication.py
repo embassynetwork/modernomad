c5d797bbc3378aa6cd1185758baf7fdfcaa45592
Jessy Kate Schingler <jessy@jessykate.com>
2019-01-26 20:28:24 -0800

    Various factory enhancements
    - Create an explicit known user with ID =1 that is also a superuser
    - Add a known password for all users
    - Add user 1 as a location admin to all generated locations
    - Add real timezones
    - Add meaningful check in and check out times

--- core/factory_apps/communication.py
+++ core/factory_apps/communication.py
@@ -3,6 +3,8 @@ from core import models
 from .user import UserFactory
 
 
+# EmailTemplates are the dropdown emails exposed in the location admin
+# JKS TODO this does not test the variables exposed in templates
 class EmailtemplateFactory(factory.DjangoModelFactory):
     class Meta:
         model = models.EmailTemplate
