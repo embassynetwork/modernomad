082046ae6a8deb82394b1057d143f97c604cd27d
Ben Firshman <ben@firshman.co.uk>
2019-02-11 20:01:32 -0800

    Make run_daily_tasks command
    To simplifier Heroku scheduler setup

---
+++ core/management/commands/run_daily_tasks.py
@@ -0,0 +1,22 @@
+import datetime
+from django.core.management.base import BaseCommand
+from ... import tasks
+from gather import tasks as gather_tasks
+
+
+class Command(BaseCommand):
+    help = 'Run daily scheduled tasks from Heroku Scheduler, or similar.'
+
+    def handle(self, *args, **kwargs):
+        tasks.generate_subscription_bills()
+        tasks.send_guests_residents_daily_update()
+        tasks.send_admin_daily_update()
+        tasks.send_guest_welcome()
+        tasks.send_departure_email()
+        tasks.slack_embassysf_daily()
+        tasks.slack_ams_daily()
+        tasks.slack_redvic_daily()
+        gather_tasks.events_today_reminder()
+        if datetime.date.today().weekday() == 6: # sunday
+            gather_tasks.weekly_upcoming_events()
+
