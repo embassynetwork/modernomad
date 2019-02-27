bd03326e8a4af4504c772d4f766b2b98c80992b2
Ben Firshman <ben@firshman.co.uk>
2019-02-27 15:35:23 -0800

    Reset DB before each browser test

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+import './beforeEach'
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
