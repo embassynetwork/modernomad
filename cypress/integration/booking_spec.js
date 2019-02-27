e61e1191b21229889d1ae2837739ace205931dac
Ben Firshman <ben@firshman.co.uk>
2019-02-27 15:28:47 -0800

    Add end-to-end booking & approve test

--- cypress/integration/booking_spec.js
+++ cypress/integration/booking_spec.js
@@ -2,6 +2,55 @@
 const randomstring = require("randomstring");
 
 describe("Booking a room", function() {
+  it("works for existing logged in user, adding card, then approved by admin", function() {
+    cy.login("pixel", "password");
+    cy.visit("/");
+    cy.contains("Embassy SF").click();
+    cy.contains("View all rooms").click();
+    cy.contains("Ada Lovelace").click({ force: true });
+
+    // Fill in booking form
+    cy.get("input[name=arrive]").focus();
+    // Pick next month so all the dates are available
+    cy.get(".react-datepicker__navigation--next").click({ force: true });
+    cy.get("[aria-label=day-13]").click({ force: true });
+    // Force de-select first date picker. For some reason immediately selecting second date
+    // picker causes first not to close.
+    cy.get("#network-footer").click("topLeft");
+    cy.get("input[name=depart]").focus();
+    cy.get("[aria-label=day-16]").click({ force: true });
+    cy.get("[name=purpose]").type("Work.");
+    cy.contains("Request to Book").click();
+
+    cy.contains("Your booking was submitted.");
+
+    // Fill in credit card form
+    cy.get("[data-stripe=number").type("4242424242424242");
+    cy.get("[data-stripe=cvc").type("123");
+    cy.get("[data-stripe=exp-month").type("12");
+    cy.get("[data-stripe=exp-year").type("22");
+    cy.contains("Add Card").click();
+
+    cy.contains("Your card has been saved.");
+
+    // Approve booking
+    cy.login("embassysfadmin", "password");
+    cy.visit("/locations/embassysf/manage/bookings/");
+    // Latest booking
+    cy.get("#booking-list-tab-content")
+      .find("tbody")
+      .find("a")
+      .first()
+      .click();
+    cy.contains("Charge the user's card").click();
+
+    // Dismiss email dialog
+    // TODO: emails
+    cy.get("#emailModal").find(".close").click();
+
+    cy.contains("This booking is confirmed and paid.")
+  });
+
   it("works for a new user", function() {
     cy.visit("/");
     cy.contains("Embassy SF").click();
@@ -37,28 +86,6 @@ describe("Booking a room", function() {
     cy.contains("Submit").click();
 
     cy.contains("Your booking has been submitted.");
-  });
-
-  it("works for an existing logged in user", function() {
-    cy.login("pixel", "password")
-    cy.visit("/");
-    cy.contains("Embassy SF").click();
-    cy.contains("View all rooms").click();
-    cy.contains("Ada Lovelace").click({ force: true });
-
-    cy.get("input[name=arrive]").focus();
-    // Pick next month so all the dates are available
-    cy.get(".react-datepicker__navigation--next").click({ force: true });
-    cy.get("[aria-label=day-13]").click({ force: true });
-    // Force de-select first date picker. For some reason immediately selecting second date
-    // picker causes first not to close.
-    cy.get("#network-footer").click("topLeft");
-    cy.get("input[name=depart]").focus();
-    cy.get("[aria-label=day-16]").click({ force: true });
-    cy.get("[name=purpose]").type("Work.");
-    cy.contains("Request to Book").click();
-
-    cy.contains("Your booking was submitted.");
   });
 
   it("works for an existing logged out user", function() {
