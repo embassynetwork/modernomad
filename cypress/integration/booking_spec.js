9decf042e64bfbc90e93a5afc9f9b83550ce6b3f
Ben Firshman <ben@firshman.co.uk>
2019-02-27 16:26:13 -0800

    Add browser tests for declined payments

--- cypress/integration/booking_spec.js
+++ cypress/integration/booking_spec.js
@@ -46,9 +46,89 @@ describe("Booking a room", function() {
 
     // Dismiss email dialog
     // TODO: emails
-    cy.get("#emailModal").find(".close").click();
+    cy.get("#emailModal")
+      .find(".close")
+      .click();
 
-    cy.contains("This booking is confirmed and paid.")
+    cy.contains("This booking is confirmed and paid.");
+  });
+
+  it("shows an error when credit card is declined when added", function() {
+    cy.login("pixel", "password");
+    cy.visit("/locations/embassysf/stay/");
+
+    cy.contains("Ada Lovelace").click({ force: true });
+
+    // Fill in booking form
+    cy.get("input[name=arrive]").focus();
+    // Pick next month so all the dates are available
+    cy.get(".react-datepicker__navigation--next").click({ force: true });
+    cy.get("[aria-label=day-13]").click({ force: true });
+    // Force de-select first date picker. For some reason immediately selecting second date
+    // picker causes first not to close.
+    cy.get("#network-footer").click("topLeft");
+    cy.get("input[name=depart]").focus();
+    cy.get("[aria-label=day-16]").click({ force: true });
+    cy.get("[name=purpose]").type("Work.");
+    cy.contains("Request to Book").click();
+
+    cy.contains("Your booking was submitted.");
+
+    // Fill in credit card form
+    // https://stripe.com/docs/testing#cards-responses
+    cy.get("[data-stripe=number").type("4000000000000002");
+    cy.get("[data-stripe=cvc").type("123");
+    cy.get("[data-stripe=exp-month").type("12");
+    cy.get("[data-stripe=exp-year").type("22");
+    cy.contains("Add Card").click();
+
+    cy.contains("Your card was declined.");
+  });
+
+  it("shows an error when credit card is declined at approval", function() {
+    cy.login("pixel", "password");
+    cy.visit("/locations/embassysf/stay/");
+
+    cy.contains("Ada Lovelace").click({ force: true });
+
+    // Fill in booking form
+    cy.get("input[name=arrive]").focus();
+    // Pick next month so all the dates are available
+    cy.get(".react-datepicker__navigation--next").click({ force: true });
+    cy.get("[aria-label=day-13]").click({ force: true });
+    // Force de-select first date picker. For some reason immediately selecting second date
+    // picker causes first not to close.
+    cy.get("#network-footer").click("topLeft");
+    cy.get("input[name=depart]").focus();
+    cy.get("[aria-label=day-16]").click({ force: true });
+    cy.get("[name=purpose]").type("Work.");
+    cy.contains("Request to Book").click();
+
+    cy.contains("Your booking was submitted.");
+
+    // Fill in credit card form
+    // https://stripe.com/docs/testing#cards-responses
+    cy.get("[data-stripe=number").type("4000000000000341");
+    cy.get("[data-stripe=cvc").type("123");
+    cy.get("[data-stripe=exp-month").type("12");
+    cy.get("[data-stripe=exp-year").type("22");
+    cy.contains("Add Card").click();
+
+    cy.contains("Your card has been saved.");
+
+    // Approve booking
+    cy.login("embassysfadmin", "password");
+    cy.visit("/locations/embassysf/manage/bookings/");
+    // Latest booking
+    cy.get("#booking-list-tab-content")
+      .find("tbody")
+      .find("a")
+      .first()
+      .click();
+    cy.contains("Charge the user's card").click();
+
+    cy.contains("The card was declined");
+    cy.contains("Charge the user's card"); // button to charge still exists, so it hasn't been marked as paid
   });
 
   it("works for a new user", function() {
