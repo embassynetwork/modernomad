8cb3ca98118fd9f155a46ee07ab850d7a9682e69
Ben Firshman <ben@firshman.co.uk>
2019-03-05 17:46:58 -0800

    Fix debug toolbar
    It seemed to be half implemented.

--- modernomad/settings/local.py
+++ modernomad/settings/local.py
@@ -8,3 +8,11 @@ INSTALLED_APPS = INSTALLED_APPS + [
 SECRET_KEY = 'local_development'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True if DEBUG else False,
+    'RESULTS_CACHE_SIZE': 100,
+}
