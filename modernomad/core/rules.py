373509c6e4d4f95f5159a71169615d6b5feff5f8
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-04-25 22:47:44 +0200

    Refactor location details to be class-based

---
+++ modernomad/core/rules.py
@@ -0,0 +1,20 @@
+from . import models
+import rules
+
+
+@rules.predicate
+def location_is_visible(user, location):
+    return any([
+        location.visibility == models.LOCATION_PUBLIC,
+        location.visibility == models.LOCATION_LINK
+    ])
+
+
+@rules.predicate
+def user_belongs_to_location(user, location):
+    return user in location.get_members()
+
+
+location_is_visible = location_is_visible | user_belongs_to_location
+
+rules.add_perm('location.can_view', location_is_visible)
