e86ab961ad7edca0fa5db998245c9b33ae8f2af1
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-05-25 10:05:54 +0200

    Refactor IndexOrDetail to remove graphql

--- client/js/components/booking/RoomIndexOrDetail.jsx
+++ client/js/components/booking/RoomIndexOrDetail.jsx
@@ -1,140 +1,67 @@
+import axios from 'axios'
 import React, {PropTypes} from 'react'
-import { browserHistory } from 'react-router'
 import RoomIndex from './RoomIndex'
 import RoomDetail from './RoomDetail'
-import gql from 'graphql-tag'
-import { graphql } from 'react-apollo'
 import _ from 'lodash'
 import moment from 'moment'
-import Loader from '../generic/Loader'
+import makeParam from '../generic/Utils'
 
-const resourcesQuery = gql`
-query AllResourcesForLocation($locationSlug: String!, $arrive: DateTime!, $depart: DateTime!) {
-  allLocations(slug: $locationSlug) {
-    edges {
-      node {
-        maxBookingDays
-        fees(paidByHouse: false) {
-          id
-          description
-          percentage
-          paidByHouse
-        }
-        resources(hasFutureCapacity: true) {
-          id
-          rid
-          name
-          description
-          summary
-          image
-          defaultRate
-          hasFutureDrftCapacity
-          acceptDrftTheseDates(arrive: $arrive, depart: $depart) 
-          availabilities(arrive: $arrive, depart: $depart) {
-            date
-            quantity
-          }
-        }
-      }
-    }
-  }
-}
-`;
 
 class RoomIndexOrDetailWithoutQuery extends React.Component {
   constructor(props) {
-    super()
+    super(props)
+    this.state = { rooms:this.props.rooms || [] };
+  }
+
+  componentWillMount() {
+    // Rooms need to be fetched if the page didn't start out with any rooms
+    // which happens when going from individual room to list view.
+    if (this.props.rooms == undefined) {
+      axios.get(`/locations/${this.props.location_name}/json/room/`)
+        .then(res => {
+          const rooms = res.data;
+          this.setState({ rooms: rooms });
+      });
+    }
   }
 
   renderSubComponent() {
-    const routeParams = this.props.routeParams
-    const child = this.props.children
     const sharedProps = {
       ...this.props,
-      networkLocation: this.locationData(),
       onFilterChange: this.reFilter.bind(this),
-      query: this.props.location.query,
-      loading: this.props.data.loading
     }
-
-    if (!!child) {
-      const roomID = child.props.routeParams.id
-      const room = this.oneResource(roomID)
-
-      if (room) {
-        return (
-          <RoomDetail {...sharedProps} room={room}>
-            {this.props.children}
-          </RoomDetail>
-        )
-      } else {
-        return null;
-      }
-    } else {
-      return <RoomIndex {...sharedProps} rooms={this.allResources()} />
-    }
+    return <RoomIndex {...sharedProps} rooms={this.state.rooms} />
   }
 
   reFilter(filters) {
-    const formattedDates = {arrive: filters.dates.arrive.format('MM/DD/YYYY'), depart: filters.dates.depart.format('MM/DD/YYYY')}
-    var path = '/locations/' + this.props.routeParams.location + '/stay/'
-
-    if (this.props.params.id) {
-      path = path + 'room/' + this.props.params.id
+    const formattedDates = {
+      arrive: filters.dates.arrive.format('MM/DD/YYYY'),
+      depart: filters.dates.depart.format('MM/DD/YYYY')
     }
+    var path = `/locations/${this.props.location_name}/stay/`
 
-    browserHistory.push({
-      pathname: path,
-      query: formattedDates
+    let jsonUrl = `/locations/${this.props.location_name}/json/room`
+    axios.get(jsonUrl, {params: formattedDates})
+      .then(res => {
+        const rooms = res.data;
+        this.setState({ rooms: rooms });
     })
-  }
 
-  locationData() {
-    const queryResults = this.props.data.allLocations
-    if (queryResults && queryResults.edges.length > 0) {
-      return queryResults.edges[0].node
-    } else {
-      return null
+    let location = {
+      pathname: path,
+      search: makeParam(formattedDates)
     }
-  }
 
-  allResources() {
-    const location = this.locationData()
-    if (location && location.resources) {
-      return location.resources
-    } else {
-      return []
-    }
-  }
-
-  oneResource(id) {
-    return _.find(this.allResources(), {rid: parseInt(id)})
+    this.props.history.push(location)
   }
 
   render() {
     return (
-      <Loader loading={this.props.data.loading}>
+      <div>
         {this.renderSubComponent()}
-      </Loader>
+      </div>
     )
   }
 }
 
-const RoomIndexOrDetail = graphql(resourcesQuery, {
-  options: (props) => {
-    const formatString = 'YYYY-MM-DDTHH:mm:ss.ms'
-    const parseFormat = 'MM/DD/YYYY'
-    const query = props.location.query
-    const arrive = query.arrive ? moment(query.arrive, parseFormat) : moment().startOf('day')
-    const depart = query.depart ? moment(query.depart, parseFormat) : arrive.clone().add(7, 'days')
-
-    return {
-      variables: {
-        locationSlug: props.routeParams.location,
-        arrive: arrive.format(formatString),
-        depart: depart.format(formatString)
-      }
-    }
-  }
-})(RoomIndexOrDetailWithoutQuery)
-export default RoomIndexOrDetail
+export default RoomIndexOrDetailWithoutQuery
